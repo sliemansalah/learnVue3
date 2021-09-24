280e9a7137441b8ce013e0bfcb3da70a4af062e1
sliemansalah <slieman2001@hotmail.com>
2021-09-24 15:40:23 +0300

    up..

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <Todo title="Todo App" />
-  <!-- <Another /> -->
 </template>
 
 <script>
 import Todo from './components/Todo.vue'
-import Another from './components/Another.vue'
 
 export default {
   name: 'App',
   components: {
-    Todo,
-    Another
+    Todo
   }
 }
 </script>
